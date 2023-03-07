a0e81e8310e4a0e3e47ed41216558ebd2269d3e6
masonMO25 <masonneale@gmail.com>
2023-03-07 12:14:38 -0600

    Added code from TODO

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -11,11 +11,26 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+  
+  export const putDb = async (content)  => {
+    console.log('PUT to the database');
+    const contactDb = await openDB('jate', 1);
+    const tx = contactDb.transaction('jate', 'readwrite');
+    const { objectStore } = tx;
+    const result = await objectStore.put({ id: 1, value: content });
+    console.log('🚀 - data saved to the database', result);
+  };
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-initdb();
+  export const getDb = async () => {
+    console.log('GET from the database');
+    const contactDb = await openDB('jate', 1);
+    const tx = contactDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+  };
+  
+  
+  initdb();
